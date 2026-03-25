fbe6b9038508ea79d9f8aa32ad3738759dba880c
Octavio Chamblas <octaviochamblasnavarrete@gmail.com>
2026-03-25 10:34:08 -0300

    refactor(content): simplificar formulario de modulos

--- apps/content/forms/module_forms.py
+++ apps/content/forms/module_forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from apps.content.models import Level, Module, Topic
+from apps.content.models import Module
 
 
 class ModuleForm(forms.ModelForm):
@@ -8,36 +8,8 @@ class ModuleForm(forms.ModelForm):
         fields = [
             "title",
             "subject",
-            "topic",
-            "levels",
             "objective",
             "description",
             "order",
             "is_published",
         ]
-        widgets = {
-            "levels": forms.CheckboxSelectMultiple,
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields["topic"].queryset = Topic.objects.none()
-        self.fields["levels"].queryset = Level.objects.filter(
-            is_active=True
-        ).order_by("order", "name")
-
-        if "subject" in self.data:
-            try:
-                subject_id = int(self.data.get("subject"))
-                self.fields["topic"].queryset = Topic.objects.filter(
-                    subject_id=subject_id,
-                    is_active=True,
-                ).order_by("name")
-            except (ValueError, TypeError):
-                pass
-        elif self.instance.pk and self.instance.subject:
-            self.fields["topic"].queryset = Topic.objects.filter(
-                subject=self.instance.subject,
-                is_active=True,
-            ).order_by("name")
