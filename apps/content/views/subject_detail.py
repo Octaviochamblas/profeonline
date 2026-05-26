5a54c08dd8d39bf7ef84d0d6b937e92360092904
Octaviochamblas <octaviochamblasnavarrete@gmail.com>
2026-05-26 01:13:47 -0400

    Mejorar detalle de asignaturas y niveles

--- apps/content/views/subject_detail.py
+++ apps/content/views/subject_detail.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Q
 from django.views.generic import DetailView
 
 from apps.content.models import Level, Resource, Subject, Topic
@@ -13,21 +13,7 @@ class SubjectDetailView(DetailView):
     slug_url_kwarg = "slug"
 
     def get_queryset(self):
-        queryset = Subject.objects.select_related("area").prefetch_related(
-            Prefetch(
-                "resources",
-                queryset=Resource.objects.filter(is_published=True).select_related(
-                    "subject",
-                    "topic",
-                ).prefetch_related("levels"),
-                to_attr="published_resources",
-            ),
-            Prefetch(
-                "topics",
-                queryset=Topic.objects.filter(is_active=True).order_by("name"),
-                to_attr="active_topics",
-            ),
-        )
+        queryset = Subject.objects.select_related("area")
 
         if self.request.user.is_authenticated and self.request.user.is_superuser:
             return queryset
@@ -37,19 +23,38 @@ class SubjectDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         subject = self.object
+
         breadcrumbs = build_breadcrumbs(
             self.request,
             ("Asignaturas", "content:subject_list", None),
             (subject.name, None, None),
         )
-
         context["breadcrumbs"] = breadcrumbs
-        context["resources"] = getattr(subject, "published_resources", [])
-        context["topics"] = getattr(subject, "active_topics", [])
+
+        # Resource search within this subject
+        q = self.request.GET.get("q", "").strip()
+        context["selected_q"] = q
+        resources_qs = Resource.objects.filter(
+            subject=subject,
+            is_published=True,
+        ).select_related("topic").prefetch_related("levels").order_by("title")
+        if q:
+            resources_qs = resources_qs.filter(
+                Q(title__icontains=q) | Q(description__icontains=q)
+            )
+        context["resources"] = resources_qs
+
+        # Active topics for this subject
+        context["topics"] = Topic.objects.filter(
+            subject=subject, is_active=True
+        ).order_by("name")
+
+        # Levels that have resources in this subject
         context["levels"] = Level.objects.filter(
             is_active=True,
             resources__subject=subject,
             resources__is_published=True,
         ).distinct().order_by("order", "name")
+
         context["structured_data_json_list"] = [breadcrumb_schema(breadcrumbs)]
         return context
