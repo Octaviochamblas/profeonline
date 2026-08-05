963ec606de24ca41228f0e90a0ea1909b7577950
Octaviochamblas <octaviochamblasnavarrete@gmail.com>
2026-08-05 15:53:44 -0400

    content(03.09): actualiza los 65 recursos de Desigualdades e Inecuaciones al estándar de 12 secciones

---
+++ apps/content/migrations/0064_load_node_03_09_content.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+from django.core.management import call_command
+
+
+def load_node_03_09_content_in_prod(apps, schema_editor):
+    call_command("load_node_content", dir="docs/conocimiento/contenido", verbosity=0)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("content", "0063_load_node_03_08_content"),
+    ]
+
+    operations = [
+        migrations.RunPython(load_node_03_09_content_in_prod, reverse_code=migrations.RunPython.noop),
+    ]
