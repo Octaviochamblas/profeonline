249eb68457023054c6660404683e0a7787c54a17
Octaviochamblas <octaviochamblasnavarrete@gmail.com>
2026-08-05 16:09:31 -0400

    content(03.14): actualiza los 50 recursos de Función Potencia al estándar de 12 secciones

---
+++ apps/content/migrations/0069_load_node_03_14_content.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+from django.core.management import call_command
+
+
+def load_node_03_14_content_in_prod(apps, schema_editor):
+    call_command("load_node_content", dir="docs/conocimiento/contenido", verbosity=0)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("content", "0068_load_node_03_13_content"),
+    ]
+
+    operations = [
+        migrations.RunPython(load_node_03_14_content_in_prod, reverse_code=migrations.RunPython.noop),
+    ]
